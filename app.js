9b90940f213749db20fdbf6c46635cc8ed7dbb09
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-07-09 21:01:01 +0100

    template

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 new Vue({
-    el:'#vue-app',
-    data:{
-        name:'AImune'
-    }
-});
+      el: '#app',
+      data: {
+        greeting: 'Welcome to your Vue.js app!',
+        docsURL: 'http://vuejs.org/guide/',
+        discordURL: 'https://chat.vuejs.org',
+        forumURL: 'http://forum.vuejs.org/'
+      },
+      methods: {
+        humanizeURL: function (url) {
+          return url
+            .replace(/^https?:\/\//, '')
+            .replace(/\/$/, '')
+        }
+      }
+    })
