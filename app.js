2b9c7bc25a8fca80cabaf14be0c87275b764e8ea
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-07-10 05:31:34 +0100

    mongoose

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
+const mongoose = require('mongoose');
+
+Vue.component('modal', {
+  template: '#modal-template'
+})
+
 
 new Vue({
       el: '#vue-app',
       data: {
         log: false,
-        user:'TEST',
+        username:'Username',
+        password:'Password',
+        user:'',
         userid:''
+      },
+      methods:{
+        logactiondesc : function(){
+          if(!this.log){
+            return 'Login'
+          }else{
+            return 'Logout'
+          }
+        },
+        logaction : function(){
+          if(!this.log){
+            this.login()
+          }else{
+            this.logout()
+          }
+        },
+        login: function(){
+          try{
+            mongoose.connect('mongodb://'+this.username+':'+this.password+'@localhost/admin?authSource=admin');
+            this.log = true;
+          }catch{
+            mongoose.disconnect();
+            this.logout()
+          }
+
+        },
+        logout: function(){
+          this.username = 'Username';
+          this.password = 'Password';
+          this.log = false;
+
+        },
+        greeting: function(){
+          return 'Welcome, '+this.username+'!'
+        }
+
       }
     });
