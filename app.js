f71b73972fd3ef77de25f88587e7f43864a3442c
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-07-10 04:04:32 +0100

    kfjlS

--- app.js
+++ app.js
@@ -1,44 +1,9 @@
+
 new Vue({
-      el: '#app',
+      el: '#vue-app',
       data: {
         log: false,
-        user:"TEST",
-        userid:""
-      },
-      methods: {
-
-        logaction: function(){
-          if(this.log){
-            this.logout()
-          }else{
-            this.login()
-          }
-        },
-
-        logactiondesc: function(){
-          if(this.log){
-            return 'Logout';
-          }else{
-            return 'Login';
-          }
-        },
-
-        logout: function(){
-          this.log = false;
-          this.user = "";
-          this.userid = "";
-        },
-
-        login: function (user, pass) {
-          
-              try{
-
-
-              }catch{
-
-              
-              }
-
-        }
+        user:'TEST',
+        userid:''
       }
-    })
+    });
