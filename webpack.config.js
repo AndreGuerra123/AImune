e5aeed1ff772868c000232dccf3d2bf86f607e29
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-07-14 19:29:08 +0100

    reconfig webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,17 @@
+const path = require('path');
+
 module.exports = {
-    entry: './app.js',
-    output: './bundle.js',
-    mode:'production'
-  }
+    mode: "production", // "production" | "development" | "none"
+    // Chosen mode tells webpack to use its built-in optimizations accordingly.
+    entry: "./app.js", // string | object | array
+    // defaults to ./src
+    // Here the application starts executing
+    // and webpack starts bundling
+    output: {
+        // options related to how webpack emits results
+        path: path.resolve(__dirname, "dist"), // string
+        // the target directory for all output files
+        // must be an absolute path (use the Node.js path module)
+        filename: "bundle.js", // string
+    }
+}
